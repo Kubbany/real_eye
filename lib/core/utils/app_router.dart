55b85b5a834ced894a3484790b8c50e40d78909d
Kubbany <2021170289@cis.asu.edu.eg>
2025-03-16 15:31:50 +0200

    routes added

---
+++ lib/core/utils/app_router.dart
@@ -0,0 +1,13 @@
+abstract class AppRouter {
+  static const kLoginView = "/loginView";
+  static const kRegisterView = "/registerView";
+  static const kHomeView = "/homeView";
+  static const kDeepFakeDetectionView = "/deepFakeDetectionView";
+  static const kAboutUsView = "/aboutUsView";
+  static const kContactUsView = "/contactUsView";
+  static const kDetectDeepFakeView = "/detectDeepFakeView";
+  static const kKnowledgeCenterView = "/knowledgeCenterView";
+  static const kFAQView = "/fAQView";
+
+}
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const RealEye());
+}
+
+class RealEye extends StatelessWidget {
+  const RealEye({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
