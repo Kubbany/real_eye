e43a520b9485de664ea12593cfe6583095eed949
Kubbany <2021170289@cis.asu.edu.eg>
2025-03-16 18:59:30 +0200

    adding font family

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:real_eye/core/utils/app_router.dart';
 
 void main() {
@@ -12,7 +13,11 @@ class RealEye extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(),
+      theme: ThemeData.light().copyWith(
+        textTheme: GoogleFonts.robotoTextTheme(
+          ThemeData.light().textTheme,
+        ),
+      ),
       routerConfig: AppRouter.router,
     );
   }
